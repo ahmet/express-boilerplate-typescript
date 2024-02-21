eba560ad4584fb5145817b655081cf89393ef200
Ahmet Aygün <me@ahmetaygun.net>
2024-02-21 22:08:58 +0300

    added eslint-plugin-jest

--- eslint.config.js
+++ eslint.config.js
@@ -3,13 +3,18 @@
 import eslint from '@eslint/js'
 import tseslint from 'typescript-eslint'
 import prettierConfig from 'eslint-config-prettier'
+import jestPlugin from 'eslint-plugin-jest'
 import importSort from 'eslint-plugin-simple-import-sort'
-
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
+  {
+    plugins: {
+      jest: jestPlugin.configs.recommended
+    }
+  },
   prettierConfig,
   {
     languageOptions: {
